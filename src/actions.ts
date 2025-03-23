9648f8bd1d97a0c9f852dd215d75ed625189ca3a
SutuSebastian <sebiitv@gmail.com>
2025-03-23 17:55:48 +0200

    add prettier scripts; format code

--- src/actions.ts
+++ src/actions.ts
@@ -1,7 +1,7 @@
-'use server';
+"use server";
 
-export let name = '';
+export let name = "";
 
 export function action(formData: FormData) {
-  name = (formData.get('name') as string) || '';
+  name = (formData.get("name") as string) || "";
 }
--- src/client.tsx
+++ src/client.tsx
@@ -1,19 +1,22 @@
 "use client-entry";
 
-import type {ReactNode} from 'react';
-import {hydrate, fetchRSC} from '@parcel/rsc/client';
+import type { ReactNode } from "react";
+import { hydrate, fetchRSC } from "@parcel/rsc/client";
 
 let updateRoot = hydrate({
   // Setup a callback to perform server actions.
   // This sends a POST request to the server, and updates the page with the response.
   async callServer(id, args) {
-    let {result, root} = await fetchRSC<{root: ReactNode, result: any}>(location.pathname, {
-      method: 'POST',
-      headers: {
-        'rsc-action-id': id,
+    let { result, root } = await fetchRSC<{ root: ReactNode; result: any }>(
+      location.pathname,
+      {
+        method: "POST",
+        headers: {
+          "rsc-action-id": id,
+        },
+        body: args,
       },
-      body: args,
-    });
+    );
     updateRoot(root);
     return result;
   },
@@ -21,30 +24,30 @@ let updateRoot = hydrate({
   onHmrReload() {
     navigate(location.pathname);
   },
-})
+});
 
 // A very simple router. When we navigate, we'll fetch a new RSC payload from the server,
-// and in a React transition, stream in the new page. Once complete, we'll pushState to 
+// and in a React transition, stream in the new page. Once complete, we'll pushState to
 // update the URL in the browser.
 async function navigate(pathname: string, push = false) {
   let root = await fetchRSC<ReactNode>(pathname);
   updateRoot(root, () => {
     if (push) {
-      history.pushState(null, '', pathname);
+      history.pushState(null, "", pathname);
     }
   });
 }
 
 // Intercept link clicks to perform RSC navigation.
-document.addEventListener('click', e => {
-  let link = (e.target as Element).closest('a');
+document.addEventListener("click", (e) => {
+  let link = (e.target as Element).closest("a");
   if (
     link &&
     link instanceof HTMLAnchorElement &&
     link.href &&
-    (!link.target || link.target === '_self') &&
+    (!link.target || link.target === "_self") &&
     link.origin === location.origin &&
-    !link.hasAttribute('download') &&
+    !link.hasAttribute("download") &&
     e.button === 0 && // left clicks only
     !e.metaKey && // open in new tab (mac)
     !e.ctrlKey && // open in new tab (windows)
@@ -58,6 +61,6 @@ document.addEventListener('click', e => {
 });
 
 // When the user clicks the back button, navigate with RSC.
-window.addEventListener('popstate', e => {
+window.addEventListener("popstate", (e) => {
   navigate(location.pathname);
 });
